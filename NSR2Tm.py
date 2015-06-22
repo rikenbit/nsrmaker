33911ae6c2fb279c31fc7df9bf53d7abd39b1835
Hiroki Danno <redgrapefruit@mac.com>
2015-06-22 12:54:26 +0000

    add plotting to NSR2Tm.py

--- NSR2Tm.py
+++ NSR2Tm.py
@@ -2,15 +2,17 @@
 NSR2Tm.py
 
 Usage:
-    NSR2Tm.py (-n nsrfile)
+    NSR2Tm.py (-n nsrfile) (-o outputfile)
     NSR2Tm.py -h | --help
     NSR2Tm.py -v | --version
 
 Options:
     -n nsrfile      File of NSR sequence
+    -o outputfile   File of histogram
     -h --help       Show this screen
     -v --version    Show version
 """
+
 
 from __future__ import print_function
 import pandas as pd
@@ -37,10 +39,26 @@ if __name__ == '__main__':
 
     args = docopt(__doc__, version="{0} {1}".format(NAME, VERSION))
     nsrfile = args['-n']
+    outputfile = args['-o']
 
     df_nsr = pd.read_csv(nsrfile)
     seq_nsr = df_nsr['seq'].values
+    Nmer = len(seq_nsr[0])
 
     tm_nsr = [calculate_Tm(seq) for seq in seq_nsr]
     df_nsr['Tm'] = tm_nsr
-    
+    text_str = ('Mean=' + str(round(np.mean(tm_nsr), 2)) + '\n S.D.=' +
+                str(round(np.std(tm_nsr), 2)))
+
+    # plotting
+    fig, ax = plt.subplots(1, 1, figsize=(4, 4))
+    ax.hist(tm_nsr, bins=28, range=(8, 36))
+    ax.set_xlabel('Tm of NSR [Degree Celsius]')
+    ax.set_ylabel('Number of NSR')
+    ax.set_title('Histogram for Tm of NSR') 
+    ax.text(0.95, 0.95, text_str, verticalalignment='top',
+            horizontalalignment='right',
+            transform=ax.transAxes,
+            color='black', fontsize=12)
+    plt.tight_layout()
+    plt.savefig(outputfile)
